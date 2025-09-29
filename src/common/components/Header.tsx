6bbf72ad059a619fd3a5efa8f2941330f0654e3f
Mathias Rendon <mathiassaid7@gmail.com>
2025-09-28 20:07:04 -0500

    hotfix: change responsiveness of the header in mobile devices

--- src/common/components/Header.tsx
+++ src/common/components/Header.tsx
@@ -4,14 +4,14 @@ import Pricing from '@/assets/pricing.png'
 export default function Header() {
     return (
         <>
-            <header className="h-50 w-full flex justify-between items-center select-none max-lg:px-6" draggable={false}>
+            <header className="h-50 w-full flex justify-between items-center select-none max-lg:px-6 max-lg:h-40 max-md:h-30 max-sm:h-20" draggable={false}>
                 <div className="header-left flex justify-center items-center gap-2">
                     <div className="logo-container">
                         <img src="favicon/favicon-96x96.png" alt="MIRA Logo" className="rounded-2xl size-16  select-none max-sm:size-12" draggable={false} />
                     </div>
                     <h2 className="font-bold text-3xl max-sm:text-2xl">MIRA</h2>
                 </div>
-                <div className="header-right flex justify-center items-center gap-6">
+                <div className="header-right flex justify-center items-center gap-6 max-sm:gap-4">
                     <div className="pricing max-sm:hidden">
                         <img src={Pricing} alt="Pricing - Coming soon" className="w-22 select-none mt-0.5" draggable={false} />
                     </div>
@@ -19,7 +19,7 @@ export default function Header() {
                         Login
                     </Link>
                     <Link to="/">
-                        <button className="px-8 py-2 rounded-full bg-[#FFF04B] text-black font-bold text-xl cursor-pointer active:bg-[#e7d944] transition max-sm:text-lg max-sm:px-6">
+                        <button className="px-8 py-2 rounded-full bg-[#FFF04B] text-black font-bold text-xl cursor-pointer active:bg-[#e7d944] transition max-sm:text-lg max-sm:px-4">
                             Register
                         </button>
                     </Link>
