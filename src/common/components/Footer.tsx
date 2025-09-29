c2460bfca5dee27d22e9e5807b67a2ef5c9abcaa
Mathias Rendon <mathiassaid7@gmail.com>
2025-09-28 19:56:01 -0500

    feat: add landing page with animations

---
+++ src/common/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <>
+        <footer className="flex justify-center items-center h-8">
+            <a href="https://github.com/JunLovin/MIRA" target="_blank" className="text-[#383838] hover:underline font-semibold text-sm">MIRA GitHub Repo</a>
+        </footer>
+        </>
+    )
+}
